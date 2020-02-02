1725a84921b8e0760e2dbdbcc71c17983a68ed36
thephisics101 <thephisics101@gmail.com>
2020-02-02 16:14:34 +0200

    final

--- Scenes/Laser/Laser_Emiter_L.gd
+++ Scenes/Laser/Laser_Emiter_L.gd
@@ -8,16 +8,32 @@ export (Directon) var dir = Directon.Left
 
 var needs_update = true
 
-func _ready():
-    $Line2D.add_point(Vector2(0, 0))
-
 func _physics_process(delta):
-#    if !needs_update:
-#        return
-    var ss = get_world_2d().direct_space_state
-    var res = ss.intersect_ray(self.global_position, Vector2(-1000, 0), [self])
+    $Line2D.clear_points()
+    $Line2D.add_point(Vector2(0,0))
+    
+    var nc = _get_next_colision(Vector2(0, 0), Vector2(-1, 0))
+    print(nc)
+    while nc:
+        nc = _get_next_colision(nc[0], nc[1])
+
+func _get_next_colision(vec, dir):
+    $RayCast2D.exclude_parent = true
+    $RayCast2D.position = vec + self.global_position
+    $RayCast2D.cast_to = dir.normalized()*1000 + self.global_position
+    $RayCast2D.force_raycast_update()
+    var res = $RayCast2D.get_collider()
     if res:
         var coll = res.collider
-        print("ye: ", res, "; ", coll.world_to_map(res.position - coll.global_position))
         $Line2D.add_point(res.position - $Line2D.global_position)
-        needs_update = false
+        if coll is TileMap:
+            var pos = coll.world_to_map(res.position - coll.global_position)
+            var tile = coll.get_cellv(pos)
+            if tile == -1:
+                return null
+            if tile > 5 or tile < 2:
+                return false
+            var v = Vector2(1 if tile % 2 == 1 else -1, -1 if tile <= 3 else 1)
+            var norm = dir.normalized()
+            if norm.x == v.x or norm.y == v.y:
+                return [res.position - self.global_position, norm - v]
